e14158f2d377d5d10990f7ab2ab1a414a316e2f0
Timothy Heironimus <timothyheironimus6+github@gmail.com>
2025-07-15 16:34:46 -0500

    deepseek: defeatedtrainer.asm

--- audio/music/defeatedtrainer.asm
+++ audio/music/defeatedtrainer.asm
@@ -1,261 +1,202 @@
 Music_DefeatedTrainer_Ch1::
-	tempo 224
+	tempo 112
 	volume 7, 7
-	duty_cycle 2
-	toggle_perfect_pitch
-	tempo 224
-	note_type 4, 10, 2
+	duty_cycle 3
+	vibrato 6, 2, 3
+	note_type 12, 11, 1
 	octave 4
-	note D_, 2
-	tempo 224
-	note_type 4, 10, 2
-	note D_, 2
-	note D_, 2
-	note D_, 2
-	octave 3
-	note A_, 2
-	octave 4
-	note D_, 2
-	note_type 4, 11, 3
-	note F#, 12
-	duty_cycle 1
-	tempo 224
+	note C_, 1
+	note E_, 1
+	note G_, 1
+	note B_, 1
 .mainloop:
-	note_type 4, 6, 3
-	octave 3
-	note A_, 6
-	note F#, 3
-	note A_, 3
-	note B_, 6
-	note G#, 3
-	note B_, 3
-	octave 4
-	note C#, 3
-	octave 3
-	note B_, 3
-	note A_, 3
-	note G_, 3
-	note A_, 3
-	note B_, 3
-	note A_, 3
-	note G_, 3
-	note A_, 6
-	note F#, 3
-	note A_, 3
-	note B_, 6
-	note G#, 3
-	note B_, 3
-	octave 4
-	note C#, 3
-	note D_, 3
-	note E_, 3
-	note F#, 3
-	note C#, 3
-	octave 3
-	note B_, 3
-	note A_, 3
-	octave 4
-	note C#, 3
-	octave 3
-	note A_, 6
-	note F#, 3
-	note A_, 3
-	note B_, 6
-	note G#, 3
-	note B_, 3
-	octave 4
+	note_type 12, 11, 7
+	octave 5
 	note C_, 6
-	octave 3
-	note A_, 3
 	octave 4
-	note C_, 3
-	note D_, 3
-	octave 3
-	note B_, 3
+	note A_, 6
+	note G_, 4
+	note A_, 4
+	note G_, 4
+	note F_, 4
+	note E_, 4
+	note_type 12, 11, 3
+	note F_, 2
+	note F_, 2
+	note_type 12, 11, 7
+	note G_, 4
+	note_type 12, 11, 3
+	note F_, 2
+	note F_, 2
+	note_type 12, 11, 7
+	note A_, 4
+	note_type 12, 11, 3
+	note F_, 2
+	note F_, 2
+	note_type 12, 11, 7
+	note B_, 4
+	note_type 12, 11, 3
+	note F_, 2
+	note F_, 2
+	note_type 12, 11, 7
+	octave 5
+	note C_, 4
 	octave 4
-	note D_, 6
-	note C#, 3
-	octave 3
-	note B_, 3
-	note A_, 3
-	note G_, 3
-	note F#, 3
-	note G_, 3
-	note A_, 3
-	note B_, 3
-	note A_, 3
-	note G_, 3
-	note F#, 3
-	note E_, 3
-	note F#, 3
-	note G_, 3
-	note A_, 3
-	note B_, 3
+	note B_, 6
+	note A_, 6
+	note G_, 4
+	note F_, 6
+	note E_, 6
+	note D_, 4
 	sound_loop 0, .mainloop
 
 Music_DefeatedTrainer_Ch2::
-	duty_cycle 2
-	note_type 4, 12, 3
+	duty_cycle 3
+	vibrato 8, 3, 4
+	note_type 12, 12, 1
 	octave 4
-	note A_, 2
-	note_type 4, 12, 3
-	note A_, 2
-	note A_, 2
-	note A_, 2
-	note B_, 2
+	note E_, 1
+	note G_, 1
+	note B_, 1
 	octave 5
-	note C#, 2
-	note_type 4, 12, 4
-	note D_, 12
+	note D_, 1
 .mainloop:
-	note_type 4, 8, 5
-	octave 4
-	note D_, 6
-	octave 3
-	note A_, 3
-	octave 4
-	note D_, 3
+	note_type 12, 12, 7
 	note E_, 6
-	octave 3
-	note B_, 3
+	note C_, 6
 	octave 4
-	note E_, 3
-	note F#, 3
-	note G_, 3
+	note B_, 4
+	octave 5
+	note C_, 4
+	octave 4
+	note B_, 4
+	note A_, 4
+	note G_, 4
+	note_type 12, 12, 3
+	note A_, 2
+	note A_, 2
+	note_type 12, 12, 7
+	note B_, 4
+	note_type 12, 12, 3
+	note A_, 2
+	note A_, 2
+	note_type 12, 12, 7
+	octave 5
+	note C_, 4
+	note_type 12, 12, 3
+	octave 4
+	note A_, 2
+	note A_, 2
+	note_type 12, 12, 7
+	octave 5
+	note D_, 4
+	note_type 12, 12, 3
+	octave 4
+	note A_, 2
+	note A_, 2
+	note_type 12, 12, 7
+	octave 5
+	note E_, 4
+	note D_, 6
+	note C_, 6
+	octave 4
+	note B_, 4
 	note A_, 6
-	note E_, 3
-	note F#, 3
 	note G_, 6
-	note D_, 6
-	octave 3
-	note A_, 3
-	octave 4
-	note D_, 3
-	note E_, 6
-	octave 3
-	note B_, 3
-	octave 4
-	note E_, 3
-	note F#, 3
-	note G_, 3
-	note A_, 6
-	note F#, 3
-	note G_, 3
-	note A_, 6
-	note D_, 6
-	octave 3
-	note A_, 3
-	octave 4
-	note D_, 3
-	note E_, 6
-	octave 3
-	note B_, 3
-	octave 4
-	note E_, 3
-	note F_, 6
-	note C_, 3
-	note F_, 3
-	note G_, 3
-	note D_, 3
-	note G_, 6
-	note_type 4, 7, 0
-	note F#, 12
-	note_type 4, 7, 7
-	note F#, 12
-	note_type 4, 6, 0
-	note E_, 12
-	note_type 4, 6, 7
-	note E_, 12
+	note F_, 4
 	sound_loop 0, .mainloop
 
 Music_DefeatedTrainer_Ch3::
-	note_type 4, 1, 0
-	octave 5
-	note D_, 2
-	note_type 4, 1, 0
-	note D_, 2
-	note D_, 2
+	note_type 12, 1, 9
+	octave 3
+	note G_, 1
+	rest 1
+	note B_, 1
+	rest 1
 	octave 4
-	note B_, 2
-	note A_, 2
-	note G_, 2
-	note A_, 12
-	note_type 4, 2, 1
+	note D_, 1
+	rest 1
+	note F_, 1
+	rest 1
 .mainloop:
-	note F#, 3
-	rest 3
-	note F#, 3
-	rest 3
-	note G#, 3
-	rest 3
-	note G#, 3
-	rest 3
-	note A_, 3
-	rest 3
-	note A_, 3
-	rest 3
-	note B_, 3
-	rest 3
-	note B_, 3
-	rest 3
-	note F#, 3
-	rest 3
-	note F#, 3
-	rest 3
-	note G#, 3
-	rest 3
-	note G#, 3
-	rest 3
-	note A_, 3
-	rest 3
-	note A_, 3
-	rest 3
-	octave 5
-	note C#, 3
-	rest 3
-	note C#, 3
+	note E_, 2
+	rest 2
+	note C_, 2
+	rest 2
+	note D_, 2
+	rest 2
+	octave 3
+	note B_, 2
+	rest 2
 	octave 4
-	note A_, 3
-	note F#, 3
-	octave 5
-	note D_, 3
-	octave 4
-	note F#, 3
-	rest 3
-	note G#, 3
-	octave 5
-	note E_, 3
-	octave 4
-	note G#, 3
-	rest 3
-	note A_, 3
-	octave 5
-	note F_, 3
-	octave 4
-	note A_, 3
-	rest 3
-	note B_, 3
-	octave 5
-	note G_, 3
-	octave 4
-	note B_, 3
-	note A#, 3
-	note A_, 3
-	rest 3
-	note A_, 3
-	rest 3
-	note A_, 3
-	rest 3
-	note A_, 3
-	octave 5
-	note C_, 3
-	note C#, 3
-	rest 3
-	note C#, 3
-	rest 3
-	note C#, 3
-	rest 3
-	note C#, 3
-	octave 4
-	note A_, 3
+	note C_, 2
+	rest 2
+	octave 3
+	note A_, 2
+	rest 2
+	note B_, 2
+	rest 2
+	note G_, 2
+	rest 2
+	note A_, 2
+	rest 2
+	note F_, 2
+	rest 2
+	note G_, 2
+	rest 2
+	note E_, 2
+	rest 2
+	note F_, 2
+	rest 2
+	note D_, 2
+	rest 2
+	note E_, 2
+	rest 2
+	note C_, 2
+	rest 2
+	note D_, 2
+	rest 2
+	octave 2
+	note B_, 2
+	rest 2
+	octave 3
+	note C_, 2
+	rest 2
+	octave 2
+	note A_, 2
+	rest 2
+	note B_, 2
+	rest 2
+	note G_, 2
+	rest 2
+	note A_, 2
+	rest 2
+	note F_, 2
+	rest 2
+	note G_, 2
+	rest 2
+	note E_, 2
+	rest 2
+	sound_loop 0, .mainloop
+
+Music_DefeatedTrainer_Ch4::
+	drum_speed 12
+	drum_note 17, 1
+	drum_note 17, 1
+	drum_note 18, 1
+	drum_note 18, 1
+	drum_note 19, 1
+	drum_note 19, 1
+	drum_note 18, 1
+	drum_note 18, 1
+.mainloop:
+	drum_note 17, 2
+	drum_note 18, 2
+	drum_note 19, 2
+	drum_note 18, 2
+	drum_note 17, 2
+	drum_note 18, 2
+	drum_note 19, 1
+	drum_note 19, 1
+	drum_note 18, 2
 	sound_loop 0, .mainloop
