8a92b9883e85526e904af7ade0c5ca44ca5c4923
Timothy Heironimus <timothyheironimus6+github@gmail.com>
2025-07-15 16:58:27 -0500

    deepseek: meetprofoak.asm

--- audio/music/meetprofoak.asm
+++ audio/music/meetprofoak.asm
@@ -2,415 +2,165 @@ Music_MeetProfOak_Ch1::
 	tempo 112
 	volume 7, 7
 	duty_cycle 3
-	toggle_perfect_pitch
-	note_type 12, 11, 4
+	vibrato 8, 2, 2
+	note_type 12, 10, 5
 	octave 3
-	note F#, 1
 	note B_, 1
 	octave 4
-	note D#, 1
-	note E_, 1
-	note F#, 12
-	note_type 12, 10, 2
-	octave 3
-	note E_, 6
-	note B_, 10
-	note E_, 6
-	note B_, 10
-	note E_, 6
-	note D#, 4
-	note F#, 2
-	note F#, 4
-	note E_, 6
-	note D#, 4
-	note F#, 2
-	note F#, 4
-	note E_, 6
-	note B_, 10
-	note E_, 6
-	note B_, 10
-	note E_, 6
-	note D#, 4
-	note F#, 2
-	note F#, 4
-	note E_, 6
-	note D#, 4
-	note F#, 2
-	note F#, 4
+	note C_, 1
+	note D_, 2
 .mainloop:
-	note F#, 6
 	note E_, 4
-	note A_, 2
-	note A_, 4
-	note F#, 6
-	note E_, 4
-	note A_, 2
-	note A_, 4
-	note F#, 6
-	note E_, 4
-	note G#, 2
-	note G#, 4
-	note F#, 6
-	note E_, 4
-	note G#, 2
-	note G#, 4
-	note E_, 6
-	note D#, 4
+	note D_, 2
+	note C_, 4
+	octave 3
+	note B_, 2
+	octave 4
+	note C_, 2
+	note D_, 2
+	note G_, 2
 	note F#, 2
-	note F#, 4
-	note E_, 6
-	note D#, 4
+	note G_, 2
+	note A_, 4
+	note G_, 2
 	note F#, 2
-	note F#, 4
-	note F#, 6
-	note E_, 4
-	note G#, 2
-	note G#, 4
-	note F#, 6
-	note E_, 4
-	note G#, 2
-	note G#, 4
+	note G_, 4
+	note E_, 2
+	note D_, 2
+	note C_, 4
+	octave 3
+	note B_, 2
+	octave 4
+	note C_, 2
+	note D_, 6
+	note C_, 2
+	octave 3
+	note B_, 4
+	note A_, 4
+	note G_, 2
+	note A_, 2
+	note B_, 2
+	octave 4
+	note C_, 2
 	sound_loop 0, .mainloop
 
 Music_MeetProfOak_Ch2::
-	vibrato 8, 1, 1
 	duty_cycle 2
-	note_type 12, 12, 4
-	octave 3
-	note B_, 1
+	vibrato 10, 3, 3
+	note_type 12, 12, 6
 	octave 4
-	note D#, 1
-	note F#, 1
-	note A#, 1
-	note B_, 12
-	note_type 12, 11, 2
-	octave 3
+	note G_, 1
+	note A_, 1
 	note B_, 2
-	octave 4
-	note C#, 2
-	note D#, 2
-	note E_, 4
-	note D#, 2
-	note C#, 4
-	note_type 12, 6, 4
-	octave 4
-	note B_, 2
-	octave 5
-	note C#, 2
-	note D#, 2
-	note E_, 4
-	note D#, 2
-	note C#, 4
-	note_type 12, 11, 2
-	octave 3
-	note B_, 2
-	note A_, 2
-	note G#, 2
-	note A_, 4
-	note B_, 2
-	note B_, 4
-	note_type 12, 8, 1
-	octave 4
-	note B_, 2
-	note A_, 2
-	note G#, 2
-	note A_, 4
-	note B_, 2
-	note B_, 4
-	note_type 12, 11, 2
-	octave 3
-	note B_, 2
-	octave 4
-	note C#, 2
-	note D#, 2
-	note E_, 4
-	note D#, 2
-	note C#, 4
-	note_type 12, 6, 4
-	octave 4
-	note B_, 2
-	octave 5
-	note C#, 2
-	note D#, 2
-	note E_, 4
-	note D#, 2
-	note C#, 4
-	note_type 12, 11, 2
-	octave 3
-	note B_, 2
-	note A_, 2
-	note G#, 2
-	note A_, 4
-	note B_, 2
-	note B_, 4
-	note_type 12, 8, 1
-	octave 4
-	note B_, 2
-	note A_, 2
-	note G#, 2
-	note A_, 4
-	note B_, 2
-	note B_, 4
-	note_type 12, 11, 5
 .mainloop:
-	octave 4
-	note C#, 6
-	octave 3
-	note A_, 1
-	octave 4
-	note C#, 1
-	note E_, 6
-	note C#, 1
-	note E_, 1
-	note F#, 4
-	note E_, 4
-	note D#, 4
-	note C#, 4
-	octave 3
-	note B_, 6
-	note G#, 1
-	note B_, 1
-	octave 4
-	note E_, 8
-	note_type 12, 6, 5
-	octave 4
-	note B_, 6
-	note G#, 1
-	note B_, 1
 	octave 5
-	note E_, 8
-	note_type 12, 11, 5
-	octave 3
-	note A_, 6
-	note F#, 1
-	note A_, 1
+	note C_, 4
 	octave 4
-	note D#, 8
-	note E_, 4
-	note D#, 4
-	note C#, 4
+	note B_, 2
+	note A_, 4
+	note G_, 2
+	note A_, 2
+	note B_, 2
+	octave 5
+	note D_, 2
+	note C_, 2
+	octave 4
+	note B_, 2
+	octave 5
+	note C_, 4
+	octave 4
+	note B_, 2
+	note A_, 2
+	note B_, 4
+	note G_, 2
+	note F#, 2
+	note G_, 4
+	note E_, 2
+	note F#, 2
+	note G_, 6
+	note E_, 2
+	note D_, 4
 	note C_, 4
 	octave 3
-	note B_, 6
-	note G#, 1
-	note B_, 1
+	note B_, 2
 	octave 4
-	note E_, 6
-	octave 3
-	note B_, 1
-	octave 4
-	note E_, 1
-	note_type 12, 11, 7
-	note G#, 16
+	note C_, 2
+	note D_, 2
+	note E_, 2
 	sound_loop 0, .mainloop
 
 Music_MeetProfOak_Ch3::
-	note_type 12, 1, 2
-	rest 10
-	octave 4
-	note F#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	octave 5
-	note C#, 1
-	rest 1
-	octave 4
-	note B_, 1
-	rest 5
-	octave 5
-	note E_, 4
-	rest 6
-	octave 4
-	note B_, 1
-	rest 5
-	octave 5
-	note E_, 4
-	octave 4
-	note F#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	octave 5
-	note C#, 1
-	rest 1
-	octave 4
-	note F#, 1
-	rest 1
-	note F#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note F#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note F#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note F#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note F#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note F#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note F#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note F#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note E_, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note E_, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note E_, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note E_, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note E_, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note E_, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note E_, 1
+	note_type 12, 1, 4
+	octave 3
+	note G_, 1
 	rest 1
 	note A_, 1
 	rest 1
+.mainloop:
 	note B_, 1
 	rest 1
-	note F#, 1
+	octave 4
+	note C_, 1
 	rest 1
-	note F#, 1
+	note D_, 1
 	rest 1
+	note E_, 1
+	rest 1
+	note D_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	octave 3
 	note B_, 1
 	rest 1
-	note F#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note F#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note F#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note F#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note F#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note F#, 1
+	note A_, 1
 	rest 1
 	note G_, 1
 	rest 1
-	note G#, 1
+	note A_, 1
 	rest 1
+	note B_, 1
+	rest 3
+	note G_, 1
+	rest 1
+	note B_, 1
+	rest 3
+	note A_, 1
+	rest 3
+	note G_, 1
+	rest 3
+	note F#, 1
+	rest 3
+	note G_, 1
+	rest 1
+	note A_, 1
+	rest 1
+	note B_, 1
+	rest 1
+	octave 4
+	note C_, 1
+	rest 1
+	sound_loop 0, .mainloop
+
+Music_MeetProfOak_Ch4::
+	drum_speed 12
+	rest 4
 .mainloop:
-.loop1:
-	octave 4
-	note A_, 1
-	rest 1
-	octave 5
-	note C#, 1
-	rest 1
-	octave 4
-	note A_, 1
-	rest 1
-	octave 5
-	note C#, 1
-	rest 1
-	octave 4
-	note A_, 1
-	rest 1
-	octave 5
-	note C#, 1
-	rest 1
-	octave 4
-	note A_, 1
-	rest 1
-	octave 5
-	note C#, 1
-	rest 1
-	sound_loop 2, .loop1
-.loop2:
-	octave 4
-	note G#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note G#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note G#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note G#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	sound_loop 2, .loop2
-.loop3:
-	note F#, 1
-	rest 1
-	note A_, 1
-	rest 1
-	note F#, 1
-	rest 1
-	note A_, 1
-	rest 1
-	note F#, 1
-	rest 1
-	note A_, 1
-	rest 1
-	note F#, 1
-	rest 1
-	note A_, 1
-	rest 1
-	sound_loop 2, .loop3
-.loop4:
-	note G#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note G#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note G#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	note G#, 1
-	rest 1
-	note B_, 1
-	rest 1
-	sound_loop 2, .loop4
+	drum_note 5, 2
+	drum_note 6, 2
+	drum_note 5, 2
+	drum_note 6, 2
+	drum_note 5, 2
+	drum_note 6, 2
+	drum_note 5, 2
+	drum_note 6, 2
+	drum_note 5, 2
+	drum_note 6, 2
+	drum_note 5, 2
+	drum_note 6, 2
+	drum_note 5, 2
+	drum_note 6, 1
+	drum_note 6, 1
+	drum_note 5, 2
+	drum_note 6, 2
 	sound_loop 0, .mainloop
