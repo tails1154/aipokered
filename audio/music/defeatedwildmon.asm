6adfeaed0a51e128036462f8a6261a99868e7b5d
Timothy Heironimus <timothyheironimus6+github@gmail.com>
2025-07-15 16:27:50 -0500

    deepseek: defeatedwildmon.asm

--- audio/music/defeatedwildmon.asm
+++ audio/music/defeatedwildmon.asm
@@ -1,297 +1,185 @@
 Music_DefeatedWildMon_Ch1::
-	tempo 112
+	tempo 100
 	volume 7, 7
-	execute_music
-	duty_cycle 3
-	vibrato 6, 3, 4
-	toggle_perfect_pitch
-	tempo 112
-	note_type 12, 11, 7
+	duty_cycle 2
+	vibrato 5, 1, 3
+	note_type 12, 9, 3
 	octave 3
-	note B_, 1
-	note A_, 1
-	note G#, 1
-	note F#, 1
-	note E_, 12
+	note E_, 2
+	note G_, 2
+	note B_, 2
+	octave 4
+	note D_, 2
 .mainloop:
-	note_type 12, 6, 2
-	note E_, 2
-	note F#, 2
-	note E_, 2
-	note F#, 2
-	note G#, 2
-	note G#, 2
-	note G#, 4
-	note A_, 2
-	note A_, 2
+	note_type 12, 9, 7
+	note C_, 6
+	octave 3
+	note A_, 6
+	note G_, 4
 	note A_, 4
-	note G#, 2
-	note G#, 2
-	note G#, 4
-	note E_, 2
-	note F#, 2
-	note E_, 2
-	note F#, 2
-	note G#, 2
-	note G#, 2
-	note G#, 4
-	note A_, 2
-	note A_, 2
-	note A_, 4
-	note G#, 8
+	note G_, 4
+	note F_, 4
+	note E_, 4
+	note_type 12, 9, 3
 	note F_, 2
-	note G_, 2
 	note F_, 2
-	note G_, 2
-	note A_, 2
-	note A_, 2
-	note A_, 4
-	note A#, 2
-	note A#, 2
-	note A#, 4
-	note A_, 2
-	note A_, 2
-	note A_, 4
+	note_type 12, 9, 7
+	note G_, 4
+	note_type 12, 9, 3
 	note F_, 2
-	note G_, 2
 	note F_, 2
-	note G_, 2
-	note A_, 2
-	note A_, 2
+	note_type 12, 9, 7
 	note A_, 4
-	note A#, 2
-	note A#, 2
-	note A#, 4
-	note A_, 8
+	note_type 12, 9, 3
+	note F_, 2
+	note F_, 2
+	note_type 12, 9, 7
+	note B_, 4
+	note_type 12, 9, 3
+	note F_, 2
+	note F_, 2
+	note_type 12, 9, 7
+	octave 4
+	note C_, 4
+	octave 3
+	note B_, 6
+	note A_, 6
+	note G_, 4
+	note F_, 6
+	note E_, 6
+	note D_, 4
 	sound_loop 0, .mainloop
 
 Music_DefeatedWildMon_Ch2::
-	execute_music
 	duty_cycle 2
-	note_type 12, 12, 3
+	vibrato 6, 2, 4
+	note_type 12, 11, 3
 	octave 4
-	note E_, 1
-	note F#, 1
-	note G#, 1
-	note A_, 1
-	note_type 12, 12, 7
-	note B_, 12
+	note G_, 2
+	note B_, 2
+	octave 5
+	note D_, 2
+	note F_, 2
 .mainloop:
-	note_type 12, 8, 2
-	octave 3
-	note B_, 2
-	note A_, 2
-	note G#, 2
-	note A_, 2
-	note B_, 2
-	note B_, 2
+	note_type 12, 11, 7
+	note E_, 6
+	note C_, 6
+	octave 4
 	note B_, 4
-	octave 4
-	note C#, 2
-	note C#, 2
-	note C#, 4
-	octave 3
-	note B_, 2
-	note B_, 2
-	note B_, 4
-	note B_, 2
-	note A_, 2
-	note G#, 2
-	note A_, 2
-	note B_, 2
-	note B_, 2
-	note B_, 4
-	octave 4
-	note C#, 2
-	note C#, 2
-	note C#, 4
-	octave 3
-	note B_, 8
-	octave 4
-	note C_, 2
-	octave 3
-	note A#, 2
-	note A_, 2
-	note A#, 2
-	octave 4
-	note C_, 2
-	note C_, 2
+	octave 5
 	note C_, 4
-	note D_, 2
-	note D_, 2
+	octave 4
+	note B_, 4
+	note A_, 4
+	note G_, 4
+	note_type 12, 11, 3
+	note A_, 2
+	note A_, 2
+	note_type 12, 11, 7
+	note B_, 4
+	note_type 12, 11, 3
+	note A_, 2
+	note A_, 2
+	note_type 12, 11, 7
+	octave 5
+	note C_, 4
+	note_type 12, 11, 3
+	octave 4
+	note A_, 2
+	note A_, 2
+	note_type 12, 11, 7
+	octave 5
 	note D_, 4
-	note C_, 2
-	note C_, 2
-	note C_, 4
-	note C_, 2
-	octave 3
-	note A#, 2
-	note A_, 2
-	note A#, 2
+	note_type 12, 11, 3
 	octave 4
-	note C_, 2
-	note C_, 2
-	note C_, 4
-	note D_, 2
-	note D_, 2
-	note D_, 4
-	note C_, 8
+	note A_, 2
+	note A_, 2
+	note_type 12, 11, 7
+	octave 5
+	note E_, 4
+	note D_, 6
+	note C_, 6
+	octave 4
+	note B_, 4
+	note A_, 6
+	note G_, 6
+	note F_, 4
 	sound_loop 0, .mainloop
 
 Music_DefeatedWildMon_Ch3::
-	execute_music
-	note_type 12, 2, 0
-	octave 5
+	note_type 12, 1, 0
+	octave 3
+	note C_, 1
+	rest 1
 	note E_, 1
 	rest 1
-	octave 6
-	note C#, 1
+	note G_, 1
 	rest 1
-	octave 5
 	note B_, 1
 	rest 1
-	octave 6
-	note D#, 1
-	rest 1
-	note E_, 1
-	rest 3
-	note E_, 1
-	rest 3
 .mainloop:
 	octave 4
-	note E_, 1
-	rest 1
-	octave 5
-	note E_, 1
-	rest 1
-	octave 4
-	note E_, 1
-	rest 1
-	octave 5
-	note E_, 1
-	rest 1
-	octave 4
-	note E_, 1
-	rest 3
-	octave 5
-	note E_, 1
-	rest 3
-	octave 4
-	note F#, 1
-	rest 1
-	octave 5
-	note F#, 1
-	rest 1
-	octave 4
-	note F#, 1
-	rest 3
-	note F#, 1
-	rest 1
-	octave 5
-	note D#, 1
-	rest 1
-	octave 4
-	note F#, 1
-	rest 1
-	note E_, 1
-	rest 1
-	note E_, 1
-	rest 1
-	octave 5
-	note E_, 1
-	rest 1
-	octave 4
-	note E_, 1
-	rest 1
-	octave 5
-	note E_, 1
-	rest 1
-	octave 4
-	note E_, 1
-	rest 3
-	octave 5
-	note E_, 1
-	rest 3
-	octave 4
-	note F#, 1
-	rest 1
-	octave 5
-	note F#, 1
-	rest 1
-	octave 4
-	note F#, 1
-	rest 3
-	octave 5
-	note E_, 1
-	rest 3
-	octave 4
-	note D#, 4
-	octave 4
-	note F_, 1
-	rest 1
-	octave 5
-	note F_, 1
-	rest 1
-	octave 4
-	note F_, 1
-	rest 1
-	octave 5
-	note F_, 1
-	rest 1
-	octave 4
-	note F_, 1
-	rest 3
-	octave 5
-	note F_, 1
-	rest 3
-	octave 4
-	note G_, 1
-	rest 1
-	octave 5
-	note G_, 1
-	rest 1
-	octave 4
-	note G_, 1
-	rest 3
-	note F_, 1
-	rest 1
-	octave 5
-	note F_, 1
-	rest 1
-	octave 4
-	note F_, 1
-	rest 1
-	note F_, 1
-	rest 1
-	note F_, 1
-	rest 1
-	octave 5
-	note F_, 1
-	rest 1
-	octave 4
-	note F_, 1
-	rest 1
-	octave 5
-	note F_, 1
-	rest 1
-	octave 4
-	note F_, 1
-	rest 3
-	octave 5
-	note F_, 1
-	rest 3
-	octave 4
-	note G_, 1
-	rest 1
-	octave 5
-	note G_, 1
-	rest 1
-	octave 4
-	note G_, 1
-	rest 3
-	octave 5
-	note F_, 1
-	rest 3
-	octave 4
-	note D#, 4
+	note C_, 2
+	rest 2
+	octave 3
+	note A_, 2
+	rest 2
+	note B_, 2
+	rest 2
+	note G_, 2
+	rest 2
+	note A_, 2
+	rest 2
+	note F_, 2
+	rest 2
+	note G_, 2
+	rest 2
+	note E_, 2
+	rest 2
+	note F_, 2
+	rest 2
+	note D_, 2
+	rest 2
+	note E_, 2
+	rest 2
+	note C_, 2
+	rest 2
+	note D_, 2
+	rest 2
+	octave 2
+	note B_, 2
+	rest 2
+	octave 3
+	note C_, 2
+	rest 2
+	octave 2
+	note A_, 2
+	rest 2
+	note B_, 2
+	rest 2
+	note G_, 2
+	rest 2
+	note A_, 2
+	rest 2
+	note F_, 2
+	rest 2
+	note G_, 2
+	rest 2
+	note E_, 2
+	rest 2
+	sound_loop 0, .mainloop
+
+Music_DefeatedWildMon_Ch4::
+	drum_speed 12
+	rest 16
+.mainloop:
+	drum_note 17, 4
+	drum_note 18, 4
+	drum_note 17, 4
+	drum_note 18, 4
+	drum_note 17, 4
+	drum_note 18, 4
+	drum_note 17, 2
+	drum_note 17, 2
+	drum_note 18, 4
 	sound_loop 0, .mainloop
